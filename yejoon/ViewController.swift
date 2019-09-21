89f5b996985ffa14106c7f6949189f790be9cf49
Seung-Hoon Lee <leeseunghoon88@gmail.com>
2019-09-21 13:54:41 +0900

    initialize yejoon

---
+++ yejoon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yejoon
+//
+//  Created by lee seung hoon on 2019-09-20.
+//  Copyright © 2019 lewislee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

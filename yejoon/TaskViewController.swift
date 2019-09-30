7a21763f8a53bb3a4525ac6338cf4eab8db36b84
maplecomdori <leeseunghoon88@gmail.com>
2019-09-30 06:03:31 -0400

    created instruction view controller

---
+++ yejoon/TaskViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TaskViewController.swift
+//  yejoon
+//
+//  Created by lewis on 2019-09-30.
+//  Copyright © 2019 lewislee. All rights reserved.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    var taskName = String()
+
+    override func viewWillAppear(_ animated: Bool) {
+        taskNameLabel.text = taskName
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- yejoon/ViewController.swift
+++ yejoon/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
 
-    private var data : [String] = []
+    private var data : [String] = ["팀원 연락처", "아이디 & 비번", "주보 편집", "주보 접는 기계", "성찬식", "선교 헌금", "시간대 별 작업"]
 
+    let sections : [String] = ["예배 전", "예배 중", "예배 후"]
+    let instructionSegueIdentifier = "ShowInstructionSegue"
     @IBOutlet weak var tableView: UITableView!
     
     let testDict : [String : String] = ["주보":"주보"]
@@ -19,9 +21,10 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for i in  0...10 {
-            data.append("\(i)")
-        }
+//        for i in  0...10 {
+//            data.append("\(i)")
+//        }
+        
         tableView.dataSource = self
         
     }
@@ -42,6 +45,13 @@ class ViewController: UIViewController, UITableViewDataSource {
         cell.textLabel?.text = text
         
         return cell
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == instructionSegueIdentifier, let destination = segue.destination as? InstructionViewController, let idx = tableView.indexPathForSelectedRow?.row {
+            destination.taskName = data[idx]
+        }
     }
 }
 

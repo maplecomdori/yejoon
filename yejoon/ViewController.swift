2a6f37e6db42a9828c495fcaa5a5ffd1720eeee2
maplecomdori <leeseunghoon88@gmail.com>
2019-09-29 21:33:51 -0400

    add tableview and navigation controller

--- yejoon/ViewController.swift
+++ yejoon/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    private var data : [String] = []
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let testDict : [String : String] = ["주보":"주보"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for i in  0...10 {
+            data.append("\(i)")
+        }
+        tableView.dataSource = self
+        
     }
 
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellReuseIdentifier")!
+        
+        let text = data[indexPath.row]
+        
+        cell.textLabel?.text = text
+        
+        return cell
+    }
 }
 
